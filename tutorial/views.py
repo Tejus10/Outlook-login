486fe254030597a4d60dccc4fe7869d54eaab8bc
Tejus10 <tejussingla454@gmail.com>
2020-01-30 21:13:30 +0530

    first commit

---
+++ tutorial/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from tutorial.auth_helper import get_sign_in_url, get_token_from_code
+
+def home(request):
+  context = initialize_context(request)
+
+  return render(request, 'tutorial/home.html', context)
+
+def initialize_context(request):
+  context = {}
+
+  # Check for any errors in the session
+  error = request.session.pop('flash_error', None)
+
+  if error != None:
+    context['errors'] = []
+    context['errors'].append(error)
+
+  # Check for user in the session
+  context['user'] = request.session.get('user', {'is_authenticated': False})
+  return context  
+
+def sign_in(request):
+  # Get the sign-in URL
+  sign_in_url, state = get_sign_in_url()
+  # Save the expected state so we can validate in the callback
+  request.session['auth_state'] = state
+  # Redirect to the Azure sign-in page
+  return HttpResponseRedirect(sign_in_url)
+
+def callback(request):
+  # Get the state saved in session
+  expected_state = request.session.pop('auth_state', '')
+  # Make the token request
+  token = get_token_from_code(request.get_full_path(), expected_state)
+  # Temporary! Save the response in an error so it's displayed
+  request.session['flash_error'] = { 'message': 'Token retrieved', 'debug': format(token) }
+  return HttpResponseRedirect(reverse('home')) 
